1f7807af44ef12630d79b047743146f95caf2346
MikejR2904 <e1129809@u.nus.edu>
2026-05-28 20:31:51 +0800

    Refactored codegen/

--- test_1_add.s
+++
@@ -1,9 +0,0 @@
-	.text
-	.globl	add_i64
-	.type	add_i64, @function
-add_i64:
-entry:
-	add	a2, a0, a1
-	mv	a0, a2
-	ret
-	.size	add_i64, .-add_i64
--- test_2_mul.s
+++
@@ -1,9 +0,0 @@
-	.text
-	.globl	mul_i64
-	.type	mul_i64, @function
-mul_i64:
-entry:
-	mul	a2, a0, a1
-	mv	a0, a2
-	ret
-	.size	mul_i64, .-mul_i64
